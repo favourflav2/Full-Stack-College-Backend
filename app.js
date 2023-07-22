78e9c1f9a854762e8fcb7dd7499ea24bbc392e02
favourflav <chukwagon1@gmail.com>
2023-07-22 02:31:10 -0700

    limited cors to only local host and production url

--- app.js
+++ app.js
@@ -10,13 +10,28 @@ import helmet from 'helmet'
 
 const app = express()
 
+
+
 // Middleware
 dotenv.config()
 app.use(helmet())
 app.use(morgan("dev"))
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb",extended:true}))
-app.use(cors())
+// cors options
+const whiteList = [`${process.env.PRODUCTION_URL}`,`${process.env.LOCALHOST_URL}`]
+
+var corsOptions = {
+    origin: function (origin, callback) {
+      if (whiteList .indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
+app.use(cors(corsOptions))
+
 
 const port = process.env.PORT || 5001
 
